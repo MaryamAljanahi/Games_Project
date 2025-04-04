de97236199794094ff6da39a98428429c2ab27a0
Yoomio <maryam.janahi3@gmail.com>
2025-04-04 19:48:19 +0400

    fourth commit

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
+const path = require('path') // Required to handle file paths
 const port = 8080
 
 var corsOptions = {
@@ -8,8 +9,12 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+// Serve static files (CSS, JavaScript, images)
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Serve the index.html file when accessing the root URL
 app.get('/', cors(corsOptions), function (req, res, next) {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'index.html')) // Adjust the path if your index.html is in a different location
 })
 
 app.listen(port, () => {
